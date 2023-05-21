3b4d8b7133e3585d4dea4e673d0fdfd65932c568
aquaman <swj981128@gmail.com>
2023-05-21 21:40:02 +0900

    1

--- instargram-clone/src/app/home/page.tsx
+++ instargram-clone/src/app/home/page.tsx
@@ -136,15 +136,15 @@ export default function page({}: Props) {
         <Left />
         <div className='flex-1 mr-14 ml-14 flex'>
 
-          <div className='mt-2.5 mr-16 w-screen max-w-screen-sm'>
+          <div className='mt-2.5 mr-16 w-full max-w-2xl'>
     
-            <ul className='w-full flex justify-between items-start mb-8 space-x-3 overflow-x-scroll stories bg-parent p-4 rounded drop-shadow-xl'>
+            <ul className='flex justify-between items-start mb-8 space-x-3 overflow-x-scroll stories bg-parent pt-4 rounded drop-shadow-xl'>
               {users.map((user) => (
                 <li key={user.id} className="flex flex-none flex-col items-center space-y-1">
                   <div className={user.isUpdate ? "bg-gradient-to-tr from-yellow-400 to-fuchsia-600 p-1 rounded-full" : "border border-gray-300 bg-white border-1 p-1 rounded-full"}>
                     <a href='#' className='block bg-white p-1 rounded-full relative'>
                       <img src={user.image_url}
-                      className='w-16 h-16 rounded-full object-cover' 
+                      className='w-12 h-12 rounded-full object-cover' 
                       />
                     </a>
                   </div>
@@ -158,7 +158,112 @@ export default function page({}: Props) {
             <div></div>
           </div>
 
-          <div className='bg-blue-200'></div>
+          <div className='w-full h-screen pt-8 pb-8'>
+
+            <div className='flex justify-between mt-4 mb-1'>
+              <div>
+                <img src={users[7].image_url} className="w-16 h-16 rounded-full object-cover" />
+              </div>
+              <div className='w-40 flex flex-col justify-center pl-4 text-sm text-white'>
+                <div>jj__n23</div>
+                <div>장석원</div>
+              </div>
+              <div className='flex justify-center items-center text-xs text-blue-500'>전환</div>
+            </div>
+
+            <div className='flex justify-between'>
+              <div className='text-gray-600 text-sm'>회원님을 위한 추천</div>
+              <div className='text-white text-xs flex items-center'>모두 보기</div>
+            </div>
+
+            <div className='flex flex-col'>
+              
+              <div className='pt-1 pr-1.5 pl-1.5 pb-1'>
+                <div className='flex mt-1 mb-1'>
+                  <div className='pr-3'>
+                    <img src={users[0].image_url} className="w-10 h-10 rounded-full object-cover" />
+                  </div>
+                  <div className='w-40 flex flex-col justify-center text-xs text-white'>
+                    <div>Aiony Haust</div>
+                    <div className='text-gray-600 overflow-x-hidden'>joe_33님 외 1명이 팔로우합니다</div>
+                  </div>
+                  <div className='w-10 flex justify-end items-center text-xs text-blue-500'>팔로우</div>
+                </div>
+              </div>
+
+              <div className='pt-1 pr-1.5 pl-1.5 pb-1'>
+                <div className='flex mt-1 mb-1'>
+                  <div className='pr-3'>
+                    <img src={users[1].image_url} className="w-10 h-10 rounded-full object-cover" />
+                  </div>
+                  <div className='w-40 flex flex-col justify-center text-xs text-white'>
+                    <div>joel_mott</div>
+                    <div className='text-gray-600 overflow-x-hidden'>joe_33님 외 1명이 팔로우합니다</div>
+                  </div>
+                  <div className='w-10 flex justify-end items-center text-xs text-blue-500'>팔로우</div>
+                </div>
+              </div>
+
+              <div className='pt-1 pr-1.5 pl-1.5 pb-1'>
+                <div className='flex mt-1 mb-1'>
+                  <div className='pr-3'>
+                    <img src={users[2].image_url} className="w-10 h-10 rounded-full object-cover" />
+                  </div>
+                  <div className='w-40 flex flex-col justify-center text-xs text-white'>
+                    <div>alex_suprun</div>
+                    <div className='text-gray-600 overflow-x-hidden'>회원님을 팔로우합니다.</div>
+                  </div>
+                  <div className='w-10 flex justify-end items-center text-xs text-blue-500'>팔로우</div>
+                </div>
+              </div>
+
+              <div className='pt-1 pr-1.5 pl-1.5 pb-1'>
+                <div className='flex mt-1 mb-1'>
+                  <div className='pr-3'>
+                    <img src={users[3].image_url} className="w-10 h-10 rounded-full object-cover" />
+                  </div>
+                  <div className='w-40 flex flex-col justify-center text-xs text-white'>
+                    <div>vince_fleming</div>
+                    <div className='text-gray-600 overflow-x-hidden'>회원님을 팔로우합니다.</div>
+                  </div>
+                  <div className='w-10 flex justify-end items-center text-xs text-blue-500'>팔로우</div>
+                </div>
+              </div>
+
+              <div className='pt-1 pr-1.5 pl-1.5 pb-1'>
+                <div className='flex mt-1 mb-1'>
+                  <div className='pr-3'>
+                    <img src={users[4].image_url} className="w-10 h-10 rounded-full object-cover" />
+                  </div>
+                  <div className='w-40 flex flex-col justify-center text-xs text-white'>
+                    <div>huston_wilson</div>
+                    <div className='text-gray-600 overflow-x-hidden'>회원님을 위한 추천</div>
+                  </div>
+                  <div className='w-10 flex justify-end items-center text-xs text-blue-500'>팔로우</div>
+                </div>
+              </div>
+        
+            </div>
+
+            <div className='w-full text-white'>
+              <div>
+                <ul>
+                  <li>소개</li>
+                  <li>도움말</li>
+                  <li>홍보센터</li>
+                  <li>API</li>
+                  <li>채용 정보</li>
+                  <li>개인정보처리방침</li>
+                  <li>약관</li>
+                  <li>위치</li>
+                  <li>언어</li>
+                  <li>Meta Verified</li>
+                </ul>
+              </div>
+              <span>@ 2023 INSTAGRAM FROM META</span>
+            </div>
+          </div>
+
         </div>
       </article>
     </>
