85c5b7ca7b1678c5a11bcd4b350206cd3085eeb9
aquaman <swj981128@gmail.com>
2023-06-13 20:57:34 +0900

    1

--- instagram-next-clone/app/compass/page.jsx
+++ instagram-next-clone/app/compass/page.jsx
@@ -11,9 +11,11 @@ export default function Compass() {
       <div className='xl:pl-60 pl-20'>
         <div className='grid grid-rows-3 grid-cols-3 w-full h-max px-28 pt-6 bg-black gap-1'>
           <div className='w-full h-80 bg-white rounded-sm'>
-            {}
-            <div></div>
-            <div></div>
+            {onHover ? (
+            <div>
+              
+            </div>) 
+            : (<div></div>)}
           </div>
           <div className='w-full h-80 bg-white rounded-sm'></div>
           <div className='w-full h-80 bg-white rounded-sm'></div>
