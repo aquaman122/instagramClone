d0a025d739dee6359a7ba28540fc86e2f6cdca7a
aquaman <swj981128@gmail.com>
2023-09-18 13:36:08 +0900

    1

--- instagram-react-clone/src/App.js
+++ instagram-react-clone/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Auth from './component/Auth/Auth';
+import MainPage from './component/MainPage/MainPage';
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './component/Auth/SignUp/SignUp';
 
 function App() {
   return (
     <>
-      <Auth />
+      <Routes>
+        <Route path='/' element={<Auth />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+      </Routes>
     </>
   );
 }
--- instagram-react-clone/src/component/Auth/Auth.js
+++ instagram-react-clone/src/component/Auth/Auth.js
@@ -1,9 +1,17 @@
 import './Auth.css';
+import { useNavigate } from 'react-router-dom';
+
 const Auth = () => {
+  const navigater = useNavigate();
+
+  const saveUserData = (props) => {
+    console.log(props.id);
+  }
 
   return (
     <>
-      
+      <button onClick={() => {}} items={1}>Login</button>
+      <button onSaveUserData={saveUserData}>SignUp</button>
     </>
   );
 }
